26f15d6903e4b268ecf08f7651088837765d9e28
GovarthananD <govarthananduraisamy@gmail.com>
2025-05-21 14:37:59 +0530

    indexurl

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:'https://complaintuserform.netlify.app'}));
 app.use(bodyParser.json());
 
 DB();
